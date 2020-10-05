13a141450efab1180e086b9de20eb693f58a502f
JesseSchouten <36227800+JesseSchouten@users.noreply.github.com>
2020-10-05 16:35:11 +0200

    .sql file change

--- FlaskApp/create_dashboard_database.sql
+++ FlaskApp/create_dashboard_database.sql
@@ -1,16 +1,18 @@
 #dashboard database
 CREATE DATABASE dashboard;
-
+drop table dashboard.users;
 CREATE TABLE dashboard.users(
 id INT AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(100),
 email VARCHAR(100),
-username blob,
+username BLOB not null,
 password VARCHAR(100),
 permissions VARCHAR(30) DEFAULT 'user',
 register_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
-UNIQUE KEY `u_k_1`(username)
+UNIQUE KEY `u_k_1`(username(30))
  );
+ 
+ SELECT * FROM dashboard.users;
 
 CREATE TABLE dashboard.articles (
 id INT AUTO_INCREMENT PRIMARY KEY,
